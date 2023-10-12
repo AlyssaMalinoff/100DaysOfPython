2cf8a91c1f0b36b0689baaf08b94310fb89469f1
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-10-12 09:54:42 -0700

    Cleaning up formatting and adding more comments

--- Day14_HigherLower/main.py
+++ Day14_HigherLower/main.py
@@ -2,29 +2,23 @@ from art import logo, vs
 from game_data import data
 import random
 
-"""
-TODO:
-create a game that compares the amount of followers a certain celebrity has and asks the user to guess which is higher. If the user gets it right, they get 1 point and are asked to compare 
-the winning choice to a new celebrity. this goes on until the user gets it wrong, which means the game ends.
-
-"""
-
-
-# ask the user which one they think is higher
-# compare the two follower counts of the selected celebrities
-# if user is correct, give them 1 point
-# if user is wrong, end the game.
-# if the user is correct, select another entry from the game_data to compare the winning option to
-
 
 def HigherLower():
-    # print(logo)
-    # print('Welcome to the Higher or Lower game! Here you will be presented with 2 options and you have to guess which one has a higher follower count from Instagram. Good luck :)')
-
+    print(logo)
+    print(
+        "Welcome to the Higher or Lower game! Here you will be presented with 2 options and you have to guess which one has a higher follower count from Instagram. Good luck :)"
+    )
     # Select two unique entries from the game_data list and assign them to variables for comparison
     random_values = random.sample(data, 2)
     optionOne = random_values[0]
     optionTwo = random_values[1]
+
+
+# Display the name, description and country of the entry selected
+# Ask user to select which option they think has a higher follower count (A = optionOne, B = optionTwo)
+# Compare the follower_count value for both entries
+# If user guessed correctly, give them a point, assign the correctly guessed choice to optionOne, and select a new random entry for optionTwo
+# If user was not correct, end the game
 
 
 HigherLower()
