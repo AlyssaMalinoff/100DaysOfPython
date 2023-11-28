071315eb3d4102e84e87bee502f957c2b427a14f
Alyssa Malinoff <alyssamalinoff@gmail.com>
2023-11-27 18:05:46 -0800

    forgot to format ops

--- Day15_CoffeeMachine/main.py
+++ Day15_CoffeeMachine/main.py
@@ -24,10 +24,48 @@ MENU = {
     }
 }
 
+currentFunds = 0
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-def Report:
+def check_resources(order_ingredients);
+    """Returns true when order can be fufilled, False if machine does not have enough resources"""
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item:]:
+            print(f"Sorry, please refill the [item] before ordering.")
+            return False
+    return True
+
+#this part seems kind of silly but is important to the project as a whole and i guess we GOTTA
+def coin_input():
+    """Returns the total from coins inserted"""
+    print("Please insert coins")
+    total =int(input("how many dollars?: ")) * 1.0
+    total += int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+def transaction(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if funds are insufficient."""
+    if money_received >= drink_cost:
+        change_dispensed = round(money_received - drink_cost, 2)
+        print(f"Here is your change: ${change_dispensed}")
+        global currentFunds
+        currentFunds += drink_cost
+        return True
+    else:
+        print("Sorry, thats an insufficient amount of funds. Money has been refunded.")
+        return False
+    
+def make_order(drink_name, order_ingredients):
+    """Subtracts the required ingredients from the total resources"""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+    
+
